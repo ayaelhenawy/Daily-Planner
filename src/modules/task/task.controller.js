1be2435c9cde41a72f7075a90f739132873ff9cb
ayaelhenawy <elhenawyaya212gmail.com>
2024-09-09 01:09:54 +0300

    UpdateTask in todaySchedule

--- src/modules/task/task.controller.js
+++ src/modules/task/task.controller.js
@@ -6,6 +6,14 @@ const addTask= async(req,res)=>{
     await taskModel.insertMany(req.body);
     res.json({message:"success"})
 }
+
+
+const updateTask =async(req,res)=>{
+    let task= await taskModel.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    res.json({message:"success",task})
+}
+
+
 export{
-    addTask
+    addTask, updateTask
 }
--- src/modules/task/task.router.js
+++ src/modules/task/task.router.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addTask } from './task.controller.js';
+import { addTask, updateTask } from './task.controller.js';
 
 const taskRouter=express.Router();
 
 taskRouter.post('/addTask',addTask);
+taskRouter.put('/updateTask/:id',updateTask)
 export{
     taskRouter
 }
