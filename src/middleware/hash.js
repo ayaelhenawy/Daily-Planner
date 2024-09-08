3110b5676ed3a93718262cc75fe645431a155f94
ayaelhenawy <elhenawyaya212gmail.com>
2024-09-08 09:15:34 +0300

    Hash Password

---
+++ src/middleware/hash.js
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt'
+
+// hashSync doesnot need Async Await
+export const hashpasowrd =(req,res,next)=>{
+   let hash= bcrypt.hashSync(req.body.password,8)
+    req.body.password=hash;
+    next();
+    
+}
--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { signUp } from './user.controller.js';
 import { checkEmailExist } from '../../middleware/checkEmailExist.js';
+import { hashpasowrd } from '../../middleware/hash.js';
 
 const userRouter=express.Router();
 
-userRouter.post('/signUp',checkEmailExist,signUp);
+userRouter.post('/signUp',checkEmailExist,hashpasowrd,signUp);
 
 export{
     userRouter
